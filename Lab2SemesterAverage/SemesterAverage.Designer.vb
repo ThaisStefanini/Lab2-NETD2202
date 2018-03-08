06eae976c2c1ce693259a781f80a1c8ac766340f
thais <thais@LAPTOP-TBK7SK9R>
2018-03-07 20:14:32 -0500

    Add project files.

---
+++ Lab2SemesterAverage/SemesterAverage.Designer.vb
@@ -0,0 +1,330 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSemesterAverage
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbCourse1 = New System.Windows.Forms.Label()
+        Me.lbCourse2 = New System.Windows.Forms.Label()
+        Me.lbCourse3 = New System.Windows.Forms.Label()
+        Me.lbCourse4 = New System.Windows.Forms.Label()
+        Me.lbCourse5 = New System.Windows.Forms.Label()
+        Me.lbCourse6 = New System.Windows.Forms.Label()
+        Me.tbCourse1 = New System.Windows.Forms.TextBox()
+        Me.tbCourse2 = New System.Windows.Forms.TextBox()
+        Me.tbCourse3 = New System.Windows.Forms.TextBox()
+        Me.tbCourse4 = New System.Windows.Forms.TextBox()
+        Me.tbCourse5 = New System.Windows.Forms.TextBox()
+        Me.tbCourse6 = New System.Windows.Forms.TextBox()
+        Me.lbCourse1Letter = New System.Windows.Forms.Label()
+        Me.lbCourse2Letter = New System.Windows.Forms.Label()
+        Me.lbCourse3Letter = New System.Windows.Forms.Label()
+        Me.lbCourse4Letter = New System.Windows.Forms.Label()
+        Me.lbCourse5Letter = New System.Windows.Forms.Label()
+        Me.lbCourse6Letter = New System.Windows.Forms.Label()
+        Me.lbSemesterLabel = New System.Windows.Forms.Label()
+        Me.lbSemesterAverage = New System.Windows.Forms.Label()
+        Me.lbSemesterLetter = New System.Windows.Forms.Label()
+        Me.lbMessage = New System.Windows.Forms.Label()
+        Me.btCalculate = New System.Windows.Forms.Button()
+        Me.btReset = New System.Windows.Forms.Button()
+        Me.btExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbCourse1
+        '
+        Me.lbCourse1.Location = New System.Drawing.Point(17, 22)
+        Me.lbCourse1.Name = "lbCourse1"
+        Me.lbCourse1.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse1.TabIndex = 0
+        Me.lbCourse1.Text = "Course 1"
+        '
+        'lbCourse2
+        '
+        Me.lbCourse2.Location = New System.Drawing.Point(17, 55)
+        Me.lbCourse2.Name = "lbCourse2"
+        Me.lbCourse2.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse2.TabIndex = 3
+        Me.lbCourse2.Text = "Course 2"
+        '
+        'lbCourse3
+        '
+        Me.lbCourse3.Location = New System.Drawing.Point(17, 88)
+        Me.lbCourse3.Name = "lbCourse3"
+        Me.lbCourse3.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse3.TabIndex = 6
+        Me.lbCourse3.Text = "Course 3"
+        '
+        'lbCourse4
+        '
+        Me.lbCourse4.Location = New System.Drawing.Point(17, 121)
+        Me.lbCourse4.Name = "lbCourse4"
+        Me.lbCourse4.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse4.TabIndex = 9
+        Me.lbCourse4.Text = "Course 4"
+        '
+        'lbCourse5
+        '
+        Me.lbCourse5.Location = New System.Drawing.Point(17, 154)
+        Me.lbCourse5.Name = "lbCourse5"
+        Me.lbCourse5.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse5.TabIndex = 12
+        Me.lbCourse5.Text = "Course 5"
+        '
+        'lbCourse6
+        '
+        Me.lbCourse6.Location = New System.Drawing.Point(17, 187)
+        Me.lbCourse6.Name = "lbCourse6"
+        Me.lbCourse6.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse6.TabIndex = 15
+        Me.lbCourse6.Text = "Course 6"
+        '
+        'tbCourse1
+        '
+        Me.tbCourse1.Location = New System.Drawing.Point(88, 19)
+        Me.tbCourse1.Name = "tbCourse1"
+        Me.tbCourse1.Size = New System.Drawing.Size(100, 22)
+        Me.tbCourse1.TabIndex = 1
+        Me.tbCourse1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'tbCourse2
+        '
+        Me.tbCourse2.Location = New System.Drawing.Point(88, 52)
+        Me.tbCourse2.Name = "tbCourse2"
+        Me.tbCourse2.Size = New System.Drawing.Size(100, 22)
+        Me.tbCourse2.TabIndex = 4
+        Me.tbCourse2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'tbCourse3
+        '
+        Me.tbCourse3.Location = New System.Drawing.Point(88, 85)
+        Me.tbCourse3.Name = "tbCourse3"
+        Me.tbCourse3.Size = New System.Drawing.Size(100, 22)
+        Me.tbCourse3.TabIndex = 7
+        Me.tbCourse3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'tbCourse4
+        '
+        Me.tbCourse4.Location = New System.Drawing.Point(88, 118)
+        Me.tbCourse4.Name = "tbCourse4"
+        Me.tbCourse4.Size = New System.Drawing.Size(100, 22)
+        Me.tbCourse4.TabIndex = 10
+        Me.tbCourse4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'tbCourse5
+        '
+        Me.tbCourse5.Location = New System.Drawing.Point(88, 151)
+        Me.tbCourse5.Name = "tbCourse5"
+        Me.tbCourse5.Size = New System.Drawing.Size(100, 22)
+        Me.tbCourse5.TabIndex = 13
+        Me.tbCourse5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'tbCourse6
+        '
+        Me.tbCourse6.Location = New System.Drawing.Point(88, 184)
+        Me.tbCourse6.Name = "tbCourse6"
+        Me.tbCourse6.Size = New System.Drawing.Size(100, 22)
+        Me.tbCourse6.TabIndex = 16
+        Me.tbCourse6.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lbCourse1Letter
+        '
+        Me.lbCourse1Letter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbCourse1Letter.Location = New System.Drawing.Point(213, 19)
+        Me.lbCourse1Letter.Name = "lbCourse1Letter"
+        Me.lbCourse1Letter.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse1Letter.TabIndex = 2
+        Me.lbCourse1Letter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbCourse2Letter
+        '
+        Me.lbCourse2Letter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbCourse2Letter.Location = New System.Drawing.Point(213, 52)
+        Me.lbCourse2Letter.Name = "lbCourse2Letter"
+        Me.lbCourse2Letter.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse2Letter.TabIndex = 5
+        Me.lbCourse2Letter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbCourse3Letter
+        '
+        Me.lbCourse3Letter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbCourse3Letter.Location = New System.Drawing.Point(213, 85)
+        Me.lbCourse3Letter.Name = "lbCourse3Letter"
+        Me.lbCourse3Letter.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse3Letter.TabIndex = 8
+        Me.lbCourse3Letter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbCourse4Letter
+        '
+        Me.lbCourse4Letter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbCourse4Letter.Location = New System.Drawing.Point(213, 118)
+        Me.lbCourse4Letter.Name = "lbCourse4Letter"
+        Me.lbCourse4Letter.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse4Letter.TabIndex = 11
+        Me.lbCourse4Letter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbCourse5Letter
+        '
+        Me.lbCourse5Letter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbCourse5Letter.Location = New System.Drawing.Point(213, 151)
+        Me.lbCourse5Letter.Name = "lbCourse5Letter"
+        Me.lbCourse5Letter.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse5Letter.TabIndex = 14
+        Me.lbCourse5Letter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbCourse6Letter
+        '
+        Me.lbCourse6Letter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbCourse6Letter.Location = New System.Drawing.Point(213, 187)
+        Me.lbCourse6Letter.Name = "lbCourse6Letter"
+        Me.lbCourse6Letter.Size = New System.Drawing.Size(100, 22)
+        Me.lbCourse6Letter.TabIndex = 17
+        Me.lbCourse6Letter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbSemesterLabel
+        '
+        Me.lbSemesterLabel.Location = New System.Drawing.Point(17, 220)
+        Me.lbSemesterLabel.Name = "lbSemesterLabel"
+        Me.lbSemesterLabel.Size = New System.Drawing.Size(100, 22)
+        Me.lbSemesterLabel.TabIndex = 18
+        Me.lbSemesterLabel.Text = "Semester"
+        '
+        'lbSemesterAverage
+        '
+        Me.lbSemesterAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbSemesterAverage.Location = New System.Drawing.Point(85, 220)
+        Me.lbSemesterAverage.Name = "lbSemesterAverage"
+        Me.lbSemesterAverage.Size = New System.Drawing.Size(100, 22)
+        Me.lbSemesterAverage.TabIndex = 19
+        Me.lbSemesterAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbSemesterLetter
+        '
+        Me.lbSemesterLetter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbSemesterLetter.Location = New System.Drawing.Point(213, 220)
+        Me.lbSemesterLetter.Name = "lbSemesterLetter"
+        Me.lbSemesterLetter.Size = New System.Drawing.Size(100, 22)
+        Me.lbSemesterLetter.TabIndex = 20
+        Me.lbSemesterLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbMessage
+        '
+        Me.lbMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbMessage.Location = New System.Drawing.Point(20, 254)
+        Me.lbMessage.Name = "lbMessage"
+        Me.lbMessage.Size = New System.Drawing.Size(293, 229)
+        Me.lbMessage.TabIndex = 21
+        '
+        'btCalculate
+        '
+        Me.btCalculate.Location = New System.Drawing.Point(20, 490)
+        Me.btCalculate.Name = "btCalculate"
+        Me.btCalculate.Size = New System.Drawing.Size(97, 23)
+        Me.btCalculate.TabIndex = 22
+        Me.btCalculate.Text = "&Calculate"
+        Me.btCalculate.UseVisualStyleBackColor = True
+        '
+        'btReset
+        '
+        Me.btReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btReset.Location = New System.Drawing.Point(140, 490)
+        Me.btReset.Name = "btReset"
+        Me.btReset.Size = New System.Drawing.Size(75, 23)
+        Me.btReset.TabIndex = 23
+        Me.btReset.Text = "&Reset"
+        Me.btReset.UseVisualStyleBackColor = True
+        '
+        'btExit
+        '
+        Me.btExit.Location = New System.Drawing.Point(238, 490)
+        Me.btExit.Name = "btExit"
+        Me.btExit.Size = New System.Drawing.Size(75, 23)
+        Me.btExit.TabIndex = 24
+        Me.btExit.Text = "&Exit"
+        Me.btExit.UseVisualStyleBackColor = True
+        '
+        'frmSemesterAverage
+        '
+        Me.AcceptButton = Me.btCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btReset
+        Me.ClientSize = New System.Drawing.Size(335, 523)
+        Me.Controls.Add(Me.btExit)
+        Me.Controls.Add(Me.btReset)
+        Me.Controls.Add(Me.btCalculate)
+        Me.Controls.Add(Me.lbMessage)
+        Me.Controls.Add(Me.lbSemesterLetter)
+        Me.Controls.Add(Me.lbSemesterAverage)
+        Me.Controls.Add(Me.lbSemesterLabel)
+        Me.Controls.Add(Me.lbCourse6Letter)
+        Me.Controls.Add(Me.lbCourse5Letter)
+        Me.Controls.Add(Me.lbCourse4Letter)
+        Me.Controls.Add(Me.lbCourse3Letter)
+        Me.Controls.Add(Me.lbCourse2Letter)
+        Me.Controls.Add(Me.lbCourse1Letter)
+        Me.Controls.Add(Me.tbCourse6)
+        Me.Controls.Add(Me.tbCourse5)
+        Me.Controls.Add(Me.tbCourse4)
+        Me.Controls.Add(Me.tbCourse3)
+        Me.Controls.Add(Me.tbCourse2)
+        Me.Controls.Add(Me.tbCourse1)
+        Me.Controls.Add(Me.lbCourse6)
+        Me.Controls.Add(Me.lbCourse5)
+        Me.Controls.Add(Me.lbCourse4)
+        Me.Controls.Add(Me.lbCourse3)
+        Me.Controls.Add(Me.lbCourse2)
+        Me.Controls.Add(Me.lbCourse1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmSemesterAverage"
+        Me.Text = "Semester Grades"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbCourse1 As Label
+    Friend WithEvents lbCourse2 As Label
+    Friend WithEvents lbCourse3 As Label
+    Friend WithEvents lbCourse4 As Label
+    Friend WithEvents lbCourse5 As Label
+    Friend WithEvents lbCourse6 As Label
+    Friend WithEvents tbCourse1 As TextBox
+    Friend WithEvents tbCourse2 As TextBox
+    Friend WithEvents tbCourse3 As TextBox
+    Friend WithEvents tbCourse4 As TextBox
+    Friend WithEvents tbCourse5 As TextBox
+    Friend WithEvents tbCourse6 As TextBox
+    Friend WithEvents lbCourse1Letter As Label
+    Friend WithEvents lbCourse2Letter As Label
+    Friend WithEvents lbCourse3Letter As Label
+    Friend WithEvents lbCourse4Letter As Label
+    Friend WithEvents lbCourse5Letter As Label
+    Friend WithEvents lbCourse6Letter As Label
+    Friend WithEvents lbSemesterLabel As Label
+    Friend WithEvents lbSemesterAverage As Label
+    Friend WithEvents lbSemesterLetter As Label
+    Friend WithEvents lbMessage As Label
+    Friend WithEvents btCalculate As Button
+    Friend WithEvents btReset As Button
+    Friend WithEvents btExit As Button
+End Class
---
+++ Lab2SemesterAverage/SemesterAverage.vb
@@ -0,0 +1,291 @@
+﻿Option Strict On
+''' Name:   Lab2SemesterAverage
+''' Author: Thais Stefanini
+''' Date:   16 Feb 2018
+''' Description:
+'''         A form that shows the letter for each grade inputed. 
+'''         After inputing 6 grades, this form calculate the average grade 
+'''         for semester and shows the average letter too.
+'''References used: 
+'''         https://msdn.microsoft.com/en-us/library/system.windows.forms.control.getnextcontrol(v=vs.110).aspx
+'''         http://www.dreamincode.net/forums/topic/194533-getting-the-value-of-all-textbox-in-an-array/
+Public Class frmSemesterAverage
+    ''' <summary>
+    ''' End the application.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btExit_Click(sender As Object, e As EventArgs) Handles btExit.Click
+        Close()
+    End Sub
+
+    ''' <summary>
+    ''' Clear the screen.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btReset_Click(sender As Object, e As EventArgs) Handles btReset.Click
+        ' Call a subroutine to clear all controls 
+        ' It also enables the Calculate button and the textboxes
+        ClearScreen()
+    End Sub
+
+    ''' <summary>
+    ''' Reset all labels and textboxes in the screen.
+    ''' </summary>
+    Private Sub ClearScreen()
+        ' Clear the textboxes
+        tbCourse1.Text = String.Empty
+        tbCourse2.Text = String.Empty
+        tbCourse3.Text = String.Empty
+        tbCourse4.Text = String.Empty
+        tbCourse5.Text = String.Empty
+        tbCourse6.Text = String.Empty
+
+        ' Enable the textboxes
+        tbCourse1.ReadOnly = False
+        tbCourse2.ReadOnly = False
+        tbCourse3.ReadOnly = False
+        tbCourse4.ReadOnly = False
+        tbCourse5.ReadOnly = False
+        tbCourse6.ReadOnly = False
+
+        ' Clear the labels for Letters
+        lbCourse1Letter.Text = String.Empty
+        lbCourse2Letter.Text = String.Empty
+        lbCourse3Letter.Text = String.Empty
+        lbCourse4Letter.Text = String.Empty
+        lbCourse5Letter.Text = String.Empty
+        lbCourse6Letter.Text = String.Empty
+
+        ' Clear the Semester Average results
+        lbSemesterAverage.Text = String.Empty
+        lbSemesterLetter.Text = String.Empty
+
+        ' Set focus on the first course textbox
+        tbCourse1.Focus()
+
+        ' Clear the message box
+        lbMessage.Text = String.Empty
+
+        ' Enable the Calculate button
+        btCalculate.Enabled = True
+
+    End Sub
+
+    ''' <summary>
+    ''' All textboxes will be checked by this event.
+    ''' Here the user input will be validate, if valid
+    ''' the letter will be determined and displayed in the label next to it
+    ''' If not valid a error message will be displayed in the Message Label
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub tbCourse_Leave(sender As Object, e As EventArgs) Handles tbCourse1.Leave, tbCourse6.Leave, tbCourse5.Leave, tbCourse4.Leave, tbCourse3.Leave, tbCourse2.Leave
+        Dim inputGrade As String = String.Empty
+        Dim isValid As Boolean = False
+        Dim errorMessage As String = String.Empty
+        Dim inputGradeDouble As Double = 0.0
+        Dim letterGrade As String = String.Empty
+
+        ' Clear the Message label, so a odd message is not displayed
+        lbMessage.Text = String.Empty
+
+        inputGrade = CType(sender, TextBox).Text
+
+        ' Call Subroutine to validate user input
+        ValidateGrade(inputGrade, errorMessage, isValid)
+
+        If isValid Then
+            ' if it is Valid, then the user input is a number and can be moved to a double variable
+            inputGradeDouble = Convert.ToDouble(inputGrade)
+
+            ' Call a subroutine to determine the letter for this Course
+            DetermineLetterGrade(inputGradeDouble, letterGrade)
+
+            ' Output the letter in the next control (Tab order)
+            GetNextControl(CType(sender, TextBox), True).Text = letterGrade
+
+        Else
+            ' Display error message
+            lbMessage.Text = "Please ensure that what you input" + errorMessage
+
+            ' Keep the focus In this control, And select its text
+            ' Unless the first course is blank, than move the focus to the first Textbox
+            ' This is necessary because of the Reset Button
+            If tbCourse1.Text Is String.Empty Then
+                tbCourse1.Focus()
+            Else
+                CType(sender, TextBox).Focus()
+                CType(sender, TextBox).SelectionStart = 0
+                CType(sender, TextBox).SelectionLength = CType(sender, TextBox).Text.Length
+            End If
+        End If
+
+
+    End Sub
+
+    ''' <summary>
+    ''' The Subroutine validate the user input
+    ''' It also format part of the error message, with the correct range.
+    ''' </summary>
+    ''' <param name="inputGrade"></param>
+    ''' <param name="errorMessage"></param>
+    ''' <param name="isValid"></param>
+    Private Sub ValidateGrade(inputGrade As String, ByRef errorMessage As String, ByRef isValid As Boolean)
+        ' Create constants for the range
+        Const MINIMUM = 0.0
+        Const MAXIMUM = 100
+
+        ' Create a variable to hold the value as a double
+        Dim inputGradeDouble As Double = 0.0
+
+        ' Ensure that "isValid" is False
+        isValid = False
+
+        ' First check if it is empty
+        ' Then check if it is numeric
+        ' And then check if it is in the range
+        If inputGrade IsNot String.Empty AndAlso
+            Double.TryParse(inputGrade, inputGradeDouble) AndAlso
+            inputGradeDouble >= MINIMUM And inputGradeDouble <= MAXIMUM Then
+
+            isValid = True
+        Else
+            errorMessage = " is a number between " + MINIMUM.ToString("n0") + " and " + MAXIMUM.ToString("n0") + "!"
+        End If
+
+    End Sub
+    ''' <summary>
+    ''' This subroutine only determine the letter for the grade
+    ''' </summary>
+    ''' <param name="Grade"></param>
+    ''' <param name="letterGrade"></param>
+    Private Sub DetermineLetterGrade(grade As Double, ByRef letterGrade As String)
+        ' This constants are the lower limit for this letter, letter F is for zero
+        Const LETTER_A_PLUS = 90.0
+        Const LETTER_A = 85.0
+        Const LETTER_A_MINUS = 80.0
+        Const LETTER_B_PLUS = 77.0
+        Const LETTER_B = 73.0
+        Const LETTER_B_MINUS = 70.0
+        Const LETTER_C_PLUS = 67.0
+        Const LETTER_C = 63.0
+        Const LETTER_C_MINUS = 60.0
+        Const LETTER_D_PLUS = 57.0
+        Const LETTER_D = 53.0
+        Const LETTER_D_MINUS = 50.0
+
+        ' Check from the minimum limit from the range
+        If grade >= LETTER_A_PLUS Then
+            letterGrade = "A+"
+        ElseIf grade >= LETTER_A Then
+            letterGrade = "A"
+        ElseIf grade >= LETTER_A_MINUS Then
+            letterGrade = "A-"
+        ElseIf grade >= LETTER_B_PLUS Then
+            letterGrade = "B+"
+        ElseIf grade >= LETTER_B Then
+            letterGrade = "B"
+        ElseIf grade >= LETTER_B_MINUS Then
+            letterGrade = "B-"
+        ElseIf grade >= LETTER_C_PLUS Then
+            letterGrade = "C+"
+        ElseIf grade >= LETTER_C Then
+            letterGrade = "C"
+        ElseIf grade >= LETTER_C_MINUS Then
+            letterGrade = "C-"
+        ElseIf grade >= LETTER_D_PLUS Then
+            letterGrade = "D+"
+        ElseIf grade >= LETTER_D Then
+            letterGrade = "D"
+        ElseIf grade >= LETTER_D_MINUS Then
+            letterGrade = "D-"
+        Else
+            letterGrade = "F"
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' This button validate all textboxes, to be sure that all of then is valid.
+    ''' Then it calculates the average of the grades, and 
+    ''' display the letter of the average.
+    ''' If there is an error, a message is displayed in the Message Label
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btCalculate_Click(sender As Object, e As EventArgs) Handles btCalculate.Click
+
+        ' DECLARATIONS
+        Const NUMBER_OF_COURSES = 6
+        Const ERROR_PESSAGE_PART_1 = "Please ensure that what you input in "    ' First part of the error message
+
+        Dim inputGrade As String = String.Empty
+        Dim isValid As Boolean = False
+        Dim errorMessagePart2 As String = String.Empty ' Second part of the error message
+        Dim inputGradeDouble As Double = 0.0
+        Dim errorMessage As String = String.Empty
+
+        Dim inputTotal As Double = 0.0
+        Dim averageGrade As Double = 0.0
+        Dim averageLetter As String = String.Empty
+
+        Dim errorMessageArray() As String
+        Dim errorArrayCounter As Integer = 0
+
+
+        ' PROCESS
+
+        errorMessageArray = New String(NUMBER_OF_COURSES - 1) {}
+
+        ' First the system check all controls and find the textboxes validating its text
+        For Each controlInForm As Control In Me.Controls
+
+            ' Validate the value only the Control is a textbox
+            If TypeOf controlInForm Is TextBox Then
+                inputGrade = controlInForm.Text
+                ValidateGrade(inputGrade, errorMessagePart2, isValid)
+
+                If isValid Then
+                    inputTotal += Convert.ToDouble(inputGrade) ' If valid, add the value to the total
+                Else
+                    ' Add the message of the error in a array, so it can be displayed latter
+                    errorMessageArray(errorArrayCounter) = ERROR_PESSAGE_PART_1 + GetNextControl(controlInForm, False).Text + errorMessagePart2
+                    errorArrayCounter += 1
+                End If
+
+            End If
+        Next
+
+        ' If there is a error message, then display it and do not calculate the average
+        If errorArrayCounter > 0 Then
+            errorMessage = "ERROR(S):" + vbNewLine
+            ' The message is save in the back order, so the first index has the error of the last control
+            For errorCounter As Integer = (errorArrayCounter - 1) To 0 Step -1
+                errorMessage += errorMessageArray(errorCounter) + vbNewLine
+            Next errorCounter
+            lbMessage.Text = errorMessage
+
+        Else ' If all courses are valid, calculate the average with letter and display them
+            CalculateAverage(inputTotal, averageGrade, NUMBER_OF_COURSES)
+            lbSemesterAverage.Text = averageGrade.ToString("n2")
+
+            DetermineLetterGrade(averageGrade, averageLetter)
+            lbSemesterLetter.Text = averageLetter
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Calculates the average
+    ''' </summary>
+    ''' <param name="inputTotal"></param>
+    ''' <param name="average"></param>
+    ''' <param name="NUMBER_OF_COURSES"></param>
+    Private Sub CalculateAverage(inputTotal As Double, ByRef average As Double, NUMBER_OF_COURSES As Integer)
+
+        average = inputTotal / NUMBER_OF_COURSES
+
+    End Sub
+End Class
